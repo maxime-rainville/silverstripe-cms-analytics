ff40fbb11bf72d8d6508b4e9c8b8ccfb907c85a7
bergice <andre.kiste@silverstripe.com>
2019-11-22 15:02:11 +1300

    - Make events work - Update README

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 ## Overview
 
-Allows you to add Google Analytics tracking to SilverStripe CMS.
+Allows you to add analytics tracking to SilverStripe CMS.
+
+This is abstracted, so you can choose from various analytics services, including Google Analytics.
+
+## Installation
+
+TODO: Publish on packagist
+
+`composer require maxime-rainville/silverstripe-cms-analytics`
+
+## Configuration
+
+1. Set your analytics code through `yaml` config:
+```yaml
+SilverStripe\Admin\LeftAndMain:
+  analytics_code: UA-XXXXX-1
+```
--- client/src/bundle.js
+++ client/src/bundle.js
@@ -1,31 +1,20 @@
-// require google analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
 document.addEventListener('DOMContentLoaded', () => {
+  window.ss.router((context, next) => {
+    let path = context.path;
 
-  ga('create', window.ss.analytics['primary_ua']);
+    // update the current page
+    ga('set', 'page', path);
 
-  ga('set', {
-    'dimension1': window.ss.analytics['environment'],
-    'dimension2': window.ss.analytics['cms_version'],
-    'dimension3': window.ss.analytics['cms_version_number'],
-    'dimension4': window.ss.analytics['permissions'],
-    'dimension5': window.ss.analytics['controller'],
+    // send the pageview event
+    ga('send', 'pageview', path);
+
+    // trigger the next callback
+    next();
   });
 
-  // window.ss.router('*', ({path}) => {
-  //   ga('set', 'page', path);
-  //   console.log(path);
-  //   ga('send', 'pageview', path);
-  // });
-
   // send click events
-  jQuery( "body" ).on( "click", ".btn.action", (event) => {
+  jQuery("body").on("click", ".btn.action", (event) => {
     const target = jQuery(event.currentTarget);
-    console.dir(target);
     ga(
       'send',
       'event',
@@ -36,24 +25,25 @@ document.addEventListener('DOMContentLoaded', () => {
     );
   })
 
+  // send react router events
+  // todo:
   // reactRouteRegister.add({
-   //   path: sectionConfig.url,
-   //   component: AssetAdminRouter,
-   //   indexRoute: { component: AssetAdminRouter },
-   //   childRoutes: [
-   //     {
-   //       path: 'show/:folderId/:viewAction/:fileId',
-   //       component: AssetAdminRouter,
-   //     },
-   //     {
-   //       path: 'show/:folderId/:viewAction',
-   //       component: AssetAdminRouter,
-   //     },
-   //     {
-   //       path: 'show/:folderId',
-   //       component: AssetAdminRouter,
-   //     },
-   //   ],
-   // });
-
- });
+  //   path: sectionConfig.url,
+  //   component: AssetAdminRouter,
+  //   indexRoute: { component: AssetAdminRouter },
+  //   childRoutes: [
+  //     {
+  //       path: 'show/:folderId/:viewAction/:fileId',
+  //       component: AssetAdminRouter,
+  //     },
+  //     {
+  //       path: 'show/:folderId/:viewAction',
+  //       component: AssetAdminRouter,
+  //     },
+  //     {
+  //       path: 'show/:folderId',
+  //       component: AssetAdminRouter,
+  //     },
+  //   ],
+  // });
+});
